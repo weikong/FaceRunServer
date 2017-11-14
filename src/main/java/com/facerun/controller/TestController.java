08bf8c0ae3d34f81fba2c71e452443ae12a530f9
平行线 <weikong555@gmail.com>
2017-11-14 16:01:29 +0800

    测试Controller

---
+++ src/main/java/com/facerun/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.facerun.controller;
+
+
+import com.facerun.bean.Run;
+import com.facerun.dao.RunMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户相关操作
+ */
+@Controller
+@RequestMapping("")
+public class TestController extends AbsController {
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+}
