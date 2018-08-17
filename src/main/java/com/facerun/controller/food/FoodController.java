85eb2c6f4e66d91eb497d9febc4c7995cac75bd8
平行线 <weikong555@gmail.com>
2018-08-17 11:52:24 +0800

    丫圈动态加载数据

--- src/main/java/com/facerun/controller/food/FoodController.java
+++ src/main/java/com/facerun/controller/food/FoodController.java
@@ -2,13 +2,17 @@ package com.facerun.controller.food;
 
 
 import com.facerun.bean.Account;
+import com.facerun.bean.Food;
+import com.facerun.bean.FoodExample;
 import com.facerun.bean.Record;
 import com.facerun.controller.AbsController;
 import com.facerun.dao.CustUserMapper;
+import com.facerun.dao.FoodMapper;
 import com.facerun.exception.BizException;
 import com.facerun.service.AccountService;
 import com.facerun.util.Code;
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +32,8 @@ public class FoodController extends AbsController {
 
     private static final Logger log = LoggerFactory.getLogger(FoodController.class);
 
+    @Autowired
+    private FoodMapper foodMapper;
 
     /**
      * 显示用户列表
@@ -35,14 +41,11 @@ public class FoodController extends AbsController {
     @GetMapping("/list")
     @ResponseBody
     public Object foodList(@RequestParam Map params, Model model) {
-        List<Record> list = new ArrayList<>();
-        for (int i = 0; i < 14; i++) {
-            Record record = new Record();
-            record.put("name", "狂暴鸭脖（爆辣） " + i);
-            record.put("desc", "月销量1254份 " + i);
-            record.put("price", "3.5元/根");
-            list.add(record);
-        }
+        String kind = MapUtils.getString(params,"kind","");
+        FoodExample example = new FoodExample();
+        if (StringUtils.isNotEmpty(kind))
+            example.createCriteria().andKindEqualTo(kind);
+        List<Food> list = foodMapper.selectByExample(example);
         return ajax(list);
     }
 }
