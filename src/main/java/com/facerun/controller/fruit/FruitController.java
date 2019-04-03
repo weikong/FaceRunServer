c805ef72f22855e41b92931bc16a692534123b1a
平行线 <weikong555@gmail.com>
2019-04-03 10:23:33 +0800

    添加数据表； 获取水果商品数据；

--- src/main/java/com/facerun/controller/fruit/FruitController.java
+++ src/main/java/com/facerun/controller/fruit/FruitController.java
@@ -1,13 +1,10 @@
 package com.facerun.controller.fruit;
 
 
-import com.facerun.bean.Food;
-import com.facerun.bean.FoodExample;
-import com.facerun.bean.Fruit;
-import com.facerun.bean.FruitExample;
+import com.facerun.bean.*;
 import com.facerun.controller.AbsController;
-import com.facerun.dao.FoodMapper;
-import com.facerun.dao.FruitMapper;
+import com.facerun.dao.*;
+import com.facerun.util.Code;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,7 +12,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.TextUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,29 +30,60 @@ public class FruitController extends AbsController {
 
     @Autowired
     private FruitMapper fruitMapper;
+    @Autowired
+    private CustFruitMapper custFruitMapper;
+    @Autowired
+    private FruitPriceTypeMapper fruitPriceTypeMapper;
+    @Autowired
+    private AddressMapper addressMapper;
 
     /**
-     * 查询丫圈列表数据
+     * 查询商品列表
      */
     @GetMapping("/list")
     @ResponseBody
-    public Object foodList(@RequestParam Map params, Model model) {
+    public Object fruitList(@RequestParam Map params, Model model) {
         FruitExample example = new FruitExample();
         List<Fruit> list = fruitMapper.selectByExample(example);
         return ajax(list);
     }
 
     /**
-     * 查询丫圈卤菜详情
+     * 通过商品id，查询商品详情
      */
-    @GetMapping("/detail")
+    @GetMapping("/detail_by_id")
     @ResponseBody
-    public Object foodDetail(@RequestParam Map params, Model model) {
-        int id = MapUtils.getInteger(params, "id");
-        FruitExample example = new FruitExample();
-        if (id > 0)
-            example.createCriteria().andIdEqualTo(id);
-        Fruit fruit = fruitMapper.selectByPrimaryKey(id);
-        return ajax(fruit);
+    public Object fruitDetailById(@RequestParam Map params, Model model) {
+        try {
+            int id = MapUtils.getInteger(params, "id",-1);
+            if (id <= 0)
+                return ajax(Code.PARAMS_MISS);
+            Map map = new HashMap();
+            map.put("id",id);
+            List<Map> list = custFruitMapper.queryFruitById(map);
+            if (list != null && list.size() == 1){
+                Map item = list.get(0);
+                FruitPriceTypeExample example = new FruitPriceTypeExample();
+                example.createCriteria().andFruitidEqualTo(id);
+                List<FruitPriceType> types = fruitPriceTypeMapper.selectByExample(example);
+                item.put("fpricetypes",types);
+                try {
+                    List<Address> addresses = new ArrayList<>();
+                    int account_id = MapUtils.getInteger(params, "account_id",-1);
+                    if (account_id > 0){
+                        AddressExample addressExample = new AddressExample();
+                        addressExample.createCriteria().andAccountidEqualTo(account_id);
+                        addresses = addressMapper.selectByExample(addressExample);
+                    }
+                    item.put("address",addresses);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            return ajax(list);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return ajax(Code.FAIL);
     }
 }
---
+++ src/main/java/com/facerun/dao/CustFruitMapper.java
@@ -0,0 +1,15 @@
+package com.facerun.dao;
+
+import com.facerun.bean.Circle;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 水果相关接口
+ */
+public interface CustFruitMapper {
+
+    List<Map> queryFruitById(Map params);
+
+}
